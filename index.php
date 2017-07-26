e984e8cda09c5d758816aea4dfe9a519bc385247
Hugo Alliaume <kocal@live.fr>
2017-07-26 07:41:51 +0200

    Configuring RedBean for using our SQLite database

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 use Siler\Route;
+use RedBeanPHP\R;
 
 require_once __DIR__.'/vendor/autoload.php';
+
+R::setup('sqlite:' . __DIR__ . '/database/database.sqlite');
 
 Route\get('/', function () {
     echo 'Hello World';
