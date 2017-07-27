94b8649eb7d0eb1dc231419871ce6fa17d2ccfd0
Hugo Alliaume <kocal@live.fr>
2017-07-27 07:54:14 +0200

    Add pagination system

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Illuminate\Http\Request;
+use Kilte\Pagination\Pagination;
 use Siler\Route;
 use Siler\Twig;
 
@@ -19,6 +20,37 @@ $request = Request::capture();
 $twig = Twig\init(__DIR__ . '/../views', __DIR__ . '/../cache', true);
 $twig->addExtension(new Twig_Extension_Debug());
 $twig->addFunction(new Twig_SimpleFunction('str_words', '\Illuminate\Support\Str::words'));
+$twig->addFunction(new Twig_SimpleFunction('paginate', function (Pagination $pagination, $url = '/') {
+    $output = ['<ul class="pagination">'];
+
+    foreach ($pagination->build() as $page => $item) {
+        $link = $url . '?page=' . $page;
+
+        switch ($item) {
+            case Pagination::TAG_PREVIOUS:
+            case Pagination::TAG_NEXT:
+                $output[] = '<li><a href="' . $link . '">' . $page . '</a></li>';
+                break;
+            case Pagination::TAG_CURRENT:
+                $output[] = '<li class="active"><span>' . $page . ' <span class="sr-only">(current)</span></li>';
+                break;
+            case Pagination::TAG_FIRST:
+                $output[] = '<li><a href="' . $link . '">«</a></li>';
+                break;
+            case Pagination::TAG_LAST:
+                $output[] = '<li><a href="' . $link . '">»</a></li>';
+                break;
+            case Pagination::TAG_LESS:
+            case Pagination::TAG_MORE:
+                $output[] = '<li><a href="' . $link . '">...</a></li>';
+                break;
+        }
+    }
+
+    $output [] = '</ul>';
+
+    echo implode('', $output);
+}));
 
 // Routes
 
